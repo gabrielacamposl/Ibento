d081ea652b2cf083cec3c61a697e35439820f927
Gabriela Campos <gabrielacampos1418@gmail.com>
2025-02-26 20:20:17 -0600

    Backend nueva versión with out SK

--- Ibento-back/backend/backend/settings.py
+++ Ibento-back/backend/backend/settings.py
@@ -11,19 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!um3^0fv#pe1ug-l-!#9&(3#y@fo)pjy@szmg=j#=_2tj06a%3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
