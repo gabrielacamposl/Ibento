03accc9884b707635fac24959a5626f1cd7a334d
Gabriela Campos <gabrielacampos1418@gmail.com>
2025-02-28 20:58:37 -0600

    Conexión de login con el back

--- ibentofront/src/App.jsx
+++ ibentofront/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Confirm from "./components/Confirm";
+import Login from "./components/Login";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/confirmar" element={<Confirm />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
