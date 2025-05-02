8901374702069e71ec374c6de900f1b635a1ecf5
Isaac Aarón Fierro Becerra <121728607+SackBandit@users.noreply.github.com>
2025-05-02 00:08:42 -0600

    SearchCard2

--- ibentofront/src/components/eventos/SearchCard2.tsx
+++ ibentofront/src/components/eventos/SearchCard2.tsx
@@ -86,7 +86,7 @@ export function EventCard({ id, imageUrl, title, date, location}) {
         <Link to={urls} className="bg-white rounded-lg p-1 h-auto w-full drop-shadow-xl ">
             <div className="bg-white w-full rounded-lg flex flex-row">
                 <img
-                src={`/${imageUrl[0][0]}`}
+                src={`${imageUrl}`}
 
                 className="rounded-lg object-cover w-40 h-40" 
                 alt={title}/>
