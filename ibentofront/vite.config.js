aca4d3840a6073953caf24f7866cf0fda95962e4
gabrielacamposl <gabrielacampos1418@gmail.com>
2025-05-16 09:33:59 -0600

    Cambio de rutas en iconos

--- ibentofront/vite.config.js
+++ ibentofront/vite.config.js
@@ -21,35 +21,33 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: '/ibento48x48.png', sizes: '48x48', type: 'image/png' },
-          { src: '/ibento52x52.png', sizes: '52x52', type: 'image/png' },
-          { src: '/ibento72x72.png', sizes: '72x72', type: 'image/png' },
-          { src: '/ibento144x144.png', sizes: '144x144', type: 'image/png' },
-          { src: '/ibento192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/ibento256x256.png', sizes: '256x256', type: 'image/png' },
-          { src: '/ibento384x384.png', sizes: '384x384', type: 'image/png' },
-          { src: '/ibento512x512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/icons/ibento48x48.png', sizes: '48x48', type: 'image/png' },
+          { src: '/icons/ibento52x52.png', sizes: '52x52', type: 'image/png' },
+          { src: '/icons/ibento72x72.png', sizes: '72x72', type: 'image/png' },
+          { src: '/icons/ibento144x144.png', sizes: '144x144', type: 'image/png' },
+          { src: '/icons/ibento192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/ibento256x256.png', sizes: '256x256', type: 'image/png' },
+          { src: '/icons/ibento384x384.png', sizes: '384x384', type: 'image/png' },
+          { src: '/icons/ibento512x512.png', sizes: '512x512', type: 'image/png' }
         ],
         "screenshots": [
           {
-            "src": "/images/screenshot1.png",
+            "src": "/images/screenshot1.jpeg",
             "sizes": "403x789",
             "type": "image/png",
-            "form_factor": "wide"
           },
           {
-            "src": "/images/screenshot2.png",
+            "src": "/images/screenshot2.jpeg",
             "sizes": "394x790",
             "type": "image/png"
           },
            {
-            "src": "/images/screenshot3.png",
+            "src": "/images/screenshot3.jpeg",
             "sizes": "405x787",
             "type": "image/png",
-            "form_factor": "wide"
           },
           {
-            "src": "/images/screenshot4.png",
+            "src": "/images/screenshot4.jpeg",
             "sizes": "394x795",
             "type": "image/png"
           }
