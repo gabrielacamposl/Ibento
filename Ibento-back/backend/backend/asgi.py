304d36d5463eda3d1e737ee39a96778c4a771953
Gabriela Campos <gabrielacampos1418@gmail.com>
2025-05-18 17:15:38 -0600

    Modifyig asgi.py -> sys.path

--- Ibento-back/backend/backend/asgi.py
+++ Ibento-back/backend/backend/asgi.py
@@ -1,10 +1,12 @@
 import os
+import sys
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from backend.api.consumer.routing import wsPatterns  # Ajusta según tu estructura
 
