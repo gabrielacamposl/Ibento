67ff02d325a40c74c818b4884e270889bf41b947
Gabriela Campos <gabrielacampos1418@gmail.com>
2025-05-17 22:21:59 -0600

    Main original

--- ibentofront/src/main.jsx
+++ ibentofront/src/main.jsx
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { PrimeReactProvider } from 'primereact/api';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
-
-// ---- Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('[SW] Registrado', reg))
-      .catch(err => console.error('[SW] Error al registrar', err))
-  });
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
