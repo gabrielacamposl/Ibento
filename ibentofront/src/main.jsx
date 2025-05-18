e1a12264d2e37692a52b5eee499c41757f3f213f
Gabriela Campos <gabrielacampos1418@gmail.com>
2025-05-17 22:46:17 -0600

    SW fixed

--- ibentofront/src/main.jsx
+++ ibentofront/src/main.jsx
@@ -4,9 +4,24 @@ import App from "./App";
 import "./index.css";
 import { PrimeReactProvider } from 'primereact/api';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<App />)
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// ---- Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('[SW] Registrado', reg))
+      .catch(err => console.error('[SW] Error al registrar', err))
+  });
+}
+
+
+
+
 
