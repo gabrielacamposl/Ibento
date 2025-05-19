377de07b393a88577e03487fb4c2470d224c3c53
Gabriela Campos <gabrielacampos1418@gmail.com>
2025-05-18 19:23:57 -0600

    backend. eliminated  v2

--- Ibento-back/backend/api/admin/views.py
+++ Ibento-back/backend/api/admin/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from backend.api.models import CategoriaEvento, Subcategoria, CategoriasPerfil
+from api.models import CategoriaEvento, Subcategoria, CategoriasPerfil
 from .serializers import (CategoriaEventoSerializer, 
                           SubcategoriaSerializer, 
                           CategoriaPerfilSerializer, 
