2cb683522c4a3f86eb1cd8bf35e227bcc15f9e74
Gabriela Campos <gabrielacampos1418@gmail.com>
2025-05-20 01:07:18 -0600

    Instalar dlib

---
+++ Ibento-back/render-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y cmake build-essential python3-dev
--- Ibento-back/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uvicorn backend.backend.asgi:application --host 0.0.0.0 --port 10000
