bd71828615e224cf10495ccd199467b611943af8
Isaac Aarón Fierro Becerra <isaacaaron@MacBook-Air.local>
2025-05-29 00:05:41 -0600

    Cambio de chat

--- Ibento-back/backend/api/consumer/routing.py
+++ Ibento-back/backend/api/consumer/routing.py
@@ -1,6 +1,12 @@
-from django.urls import path
+# from django.urls import path
+# from .consumer import ChatConsumer
+
+# wsPatterns = [
+#     path('ws/mensajes/<str:room_name>/', ChatConsumer.as_asgi()),
+# ]
+from django.urls import re_path
 from .consumer import ChatConsumer
 
-wsPatterns = [
-    path('ws/mensajes/<str:room_name>/', ChatConsumer.as_asgi()),
+wsPatterns = [ 
+    re_path(r'^ws/mensajes/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
 ]
